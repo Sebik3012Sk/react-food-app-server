2f50ce963d5eafe6ec20b9ae3a20daab4ea45247
Sebik3012Sk <112552885+Sebik3012Sk@users.noreply.github.com>
2023-09-05 18:39:42 +0200

    Update new Test Route

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.set("view engine","ejs");
 const apiData = require("./routes/apiData");
 const addRecipe = require("./routes/addRecipe");
 const registerUser = require("./routes/registerUser");
+const test = require("./routes/test");
 
 app.use(cors());
 app.use(express.json());
@@ -17,9 +18,10 @@ app.use(express.json());
 app.use("/",apiData);
 app.use("/",addRecipe);
 app.use("/",registerUser);
+app.use("/",test);
 
 app.get("/",(req,res) => {
-    res.render("index");
+    // res.render("index");
 })
 
 app.get("*",(req,res) => {
