82f36b142870ed803723af65d2e9de3652533a20
Nezo96 <racradoslav96@gmail.com>
2023-09-14 19:53:13 +0200

    Updated registerUser.js

--- routes/registerUser.js
+++ routes/registerUser.js
@@ -2,24 +2,39 @@ const registerUser = require("express").Router();
 const { createConnection } = require("mysql");
 
 const database = createConnection({
-    host : "localhost",
-    user : "root",
-    password : "",
-    database : "react-recipes-db"
-})
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "react-recipes-db",
+});
 
-registerUser.post("/register-user",(req,res) => {
+// registerUser.post("/register-user", (req, res) => {
+//   const { email, password } = req.body;
+//   console.log("email", email);
+//   console.log("password", password);
 
-    const { email , password } = req.body;
+//   const sql_query = `INSERT INTO users (email, password) VALUES ('${email}', '${password}')`;
+//   database.query(sql_query, (error, results, fields) => {
+//     if (error) {
+//       console.log(error);
+//     }
 
-    const sql_query = `INSERT INTO users (\`id\`, \`email\`, \`password\`) VALUES ('id','${email}', '${password}')`
-    database.query(sql_query,(err,result,fields) => {
-        if(err) {
-            return console.log(err);
-        }
+//     console.log(fields);
+//   });
+// });
 
-        return result;
-    })
-})
+registerUser.post("/register-user", (req, res) => {
+  const query = "INSERT INTO users (`email`, `password`) VALUES (?)";
+  const values = [req.body.email, req.body.password];
+
+  // req.body.password - treba zahashovat heslo
+
+  database.query(query, [values], (error, data) => {
+    if (error) {
+      return res.json(error);
+    }
+    return res.json("User has been added successfully.");
+  });
+});
 
 module.exports = registerUser;
