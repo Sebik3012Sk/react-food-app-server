620be2471a052b1f8f5f882771e82fd58f0c5557
Nezo96 <racradoslav96@gmail.com>
2023-09-14 19:52:49 +0200

    Updated apiData.js

--- routes/apiData.js
+++ routes/apiData.js
@@ -2,21 +2,31 @@ const apiData = require("express").Router();
 const { createConnection } = require("mysql");
 
 const database = createConnection({
-    host : "localhost",
-    user : "root",
-    database : "react-recipes-db",
-    password : "",
-    connectionLimit : 10
-})
+  host: "localhost",
+  user: "root",
+  database: "react-recipes-db",
+  password: "",
+  connectionLimit: 10,
+});
 
-apiData.get("/api-data",(req,res) => {
-    database.query("SELECT * FROM `recipes`",(err,result,fields) => {
-        if(err) {
-            return console.log(err);
-        }
+// apiData.get("/api-data",(req,res) => {
+//     database.query("SELECT * FROM `recipes`",(err,result,fields) => {
+//         if(err) {
+//             return console.log(err);
+//         }
 
-        return res.json(result);
-    })
-})
+//         return res.json(result);
+//     })
+// })
+
+apiData.get("/api-data", (req, res) => {
+  const query = "SELECT * FROM recipes";
+  database.query(query, (error, data) => {
+    if (error) {
+      return res.json(error);
+    }
+    return res.json(data);
+  });
+});
 
 module.exports = apiData;
