1f1724c48b71b3511b2f35ad9f62daa4b85c715f
Nezo96 <racradoslav96@gmail.com>
2023-09-19 13:55:06 +0200

    Update server

--- routes/addRecipe.js
+++ routes/addRecipe.js
@@ -2,36 +2,7 @@ const postDatadb = require("express").Router();
 const multer = require("multer");
 const database = require("../database/connection");
 const path = require("path");
-const verify = require("../middleware/verify")
-
-// const database = createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "react-recipes-db",
-// });
-
-// postDatadb.post("/post-datadb",(req,res) => {
-//     const { title , image , recipe_text } = req.body;
-//     const database = createConnection({
-//         host : "localhost",
-//         user : "root",
-//         password : "",
-//         database : "react-recipes-db"
-//     })
-
-//     console.log(title);
-
-//     const sqlQuery = `INSERT INTO recipes (\`title\`, \`image\`, \`id\`, \`recipe_text\`) VALUES ('${title}', '${image}', 'id', '${recipe_text}')`;
-
-//     database.query(sqlQuery,(err,result,fields) => {
-//         if(err) {
-//             return console.log(err);
-//         }
-
-//         console.log(result);
-//     })
-// })
+const verify = require("../middleware/verify");
 
 // Define storage for images
 const storage = multer.diskStorage({
@@ -48,16 +19,15 @@ const upload = multer({ storage: storage });
 
 // Upload recipe to database
 postDatadb.post("/post-datadb", verify, upload.single("image"), (req, res) => {
-
   // Make query
-  const query ="INSERT INTO recipes (`title`, `image`, `recipe_text`) VALUES (?)";
+  const query =
+    "INSERT INTO recipes (`title`, `image`, `recipe_text`) VALUES (?)";
 
   // Add values
   const values = [req.body.title, req.file.filename, req.body.recipeText];
 
   // Do query with query and values
   database.query(query, [values], (error, data) => {
-
     // If there is any error, return error as json
     if (error) {
       return res.json(error);
